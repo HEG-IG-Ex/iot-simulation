9ac36cab25805e02193b1d2fa433cc82f7e1a564
Gachet-A <a.gachet@dem-technologies.ch>
2022-12-10 13:44:12 +0100

    Code M5 Stack Corrigé et fonctionnel (en test)

--- anthony/server_udp_m5stack.py
+++ anthony/server_udp_m5stack.py
@@ -1,26 +1,28 @@
 import network
 import socket
 
-#Pour la connection au Wifi
-network: str = 'Christophe'
-password: str = 'alexZurcher'
+#Connecting to Wifi
+network_wifi = 'Christophe'
+password = 'alexZurcher'
+sta_if = network.WLAN(network.STA_IF)
 
 def connect_wifi():
-    sta_if = network.WLAN(network.STA_IF)
-    
     if not sta_if.isconnected():
         print("Connecting to network...")
         sta_if.active(True)
-        sta_if.connect(network, password)
+        sta_if.connect(network_wifi, password)
         while not sta_if.isconnected():
             pass
         print("Network connected !", sta_if.ifconfig())
         
 connect_wifi()
 
-self_address: str = sta_if.ifconfig()[1]
+self_address = sta_if.ifconfig()[0]
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+print(self_address)
+
+#Starting UDP server
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDPServerSocket.bind((self_address, 12345))
 
 print("UDP server started")
