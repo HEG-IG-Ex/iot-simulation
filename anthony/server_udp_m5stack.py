794b4535b14f8431320cfd7b1d25a5d707cba3fb
Gachet-A <a.gachet@dem-technologies.ch>
2022-12-13 16:56:44 +0100

    M5Stack code modified

--- anthony/server_udp_m5stack.py
+++ anthony/server_udp_m5stack.py
@@ -4,8 +4,8 @@ import time
 from m5stack import lcd, speaker, buttonA, buttonB, buttonC
 
 #Connecting to Wifi
-network_wifi = ''
-password = ''
+network_wifi = 'Christophe'
+password = 'alexZurcher'
 sta_if = network.WLAN(network.STA_IF)
 
 def connect_wifi():
@@ -38,17 +38,17 @@ while True:
     
         #M5 state depending on topic
         if topic == "/salon/pot":
-            print("Pot:", value)
+            lcd.print("Pot:", value)
         elif topic == "/salon/pir":
-            print("Pir:", value)
+            lcd.print("Présence:", value)
         elif topic == "/salon/ultra":
-            print("Ultra:", value)
+            lcd.print("Distance:", value)
         elif topic == "/salon/urgence":
             while not buttonA.isPressed():
+                lcd.setTextColor(color=lcd.CYAN, bcolor=lcd.BLACK)
                 speaker.volume(1)
-                speaker.tone(freq=1800,duration=1)
-                lcd.setTextColor(color=lcd.CYAN, bcolor=lcd.WHITE)
-                lcd.print("URGENCE !\n")
+                speaker.tone(freq=261, volume=1, duration=2)
+                lcd.print("URGENCE ! au lieu: " + value + "\n")
     UDP_server_socket.sendto("Connected to UDP server".encode('UTF-8'), coord_client)
         
 UDP_server_socket.close()
